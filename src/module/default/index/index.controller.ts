25fb09ae0570710d8aee40e92ce8b4d622769d8a
zhaochao <494979582@qq.com>
2022-06-09 22:25:46 +0800

    使用redis缓存解决高并发

--- src/module/default/index/index.controller.ts
+++ src/module/default/index/index.controller.ts
@@ -4,6 +4,7 @@ import { NavService } from '../../../service/nav/nav.service';
 import { FocusService } from '../../../service/focus/focus.service';
 import { GoodsCateService } from '../../../service/goods-cate/goods-cate.service';
 import { GoodsService } from '../../../service/goods/goods.service';
+import { CacheService } from '../../../service/cache/cache.service';
 @Controller('')
 export class IndexController {
   constructor(
@@ -11,77 +12,111 @@ export class IndexController {
     private focusService: FocusService,
     private goodsCateService: GoodsCateService,
     private goodsService: GoodsService,
+    private cacheService: CacheService,
   ) {}
   @Get()
   @Render('default/index/index')
   async index() {
     //顶部导航
-    const topNavResult = await this.navService.find({ position: 1, status: 1 });
+    let topNavResult = await this.cacheService.get('indexTopNav');
+    if (!topNavResult) {
+      topNavResult = await this.navService.find({ position: 1, status: 1 });
+      this.cacheService.set('indexTopNav', topNavResult, 60 * 60);
+    }
+
     //轮播图
-    const focusResult = await this.focusService.find({});
+    let focusResult = await this.cacheService.get('indexFocus');
+    if (!focusResult) {
+      focusResult = await this.focusService.find(
+        {},
+        {
+          sort: -1,
+        },
+      );
+      this.cacheService.set('indexFocus', focusResult, 60 * 60);
+    }
+
     //商品分类
-    const goodsCateResult = await this.goodsCateService.getModel().aggregate([
-      {
-        $lookup: {
-          from: 'goods_cate',
-          localField: '_id',
-          foreignField: 'pid',
-          as: 'items',
+    let goodsCateResult = await this.cacheService.get('indexGoodsCate');
+    if (!goodsCateResult) {
+      goodsCateResult = await this.goodsCateService.getModel().aggregate([
+        {
+          $lookup: {
+            from: 'goods_cate',
+            localField: '_id',
+            foreignField: 'pid',
+            as: 'items',
+          },
         },
-      },
-      {
-        $match: {
-          pid: '0',
-          status: 1,
+        {
+          $match: {
+            pid: '0',
+            status: 1,
+          },
         },
-      },
-    ]);
+      ]);
+      this.cacheService.set('indexGoodsCate', goodsCateResult, 60 * 60);
+    }
     //获取中间导航数据
-    let middleNavResult = await this.navService.find({
-      position: 2,
-      status: 1,
-    });
-    //middleNavResult 不可改变对象 (坑)  改为可改变对象
-    middleNavResult = JSON.parse(JSON.stringify(middleNavResult));
-    for (let i = 0; i < middleNavResult.length; i++) {
-      if (middleNavResult[i].relation) {
-        try {
-          //1. relation转化为数组
-          const tempArr = middleNavResult[i].relation
-            .replace(/，/g, ',')
-            .split(',');
-          const relationIdsArr = [];
-          //2、数组中的_id 转换成 Obejct _id
-          tempArr.forEach((value) => {
-            relationIdsArr.push(new mongoose.Types.ObjectId(value));
-          });
-          //3、数据库里面查找 _id 对应的数据
-          const relationGoodsArr = await this.goodsService.findIn(
-            {
-              _id: { $in: relationIdsArr },
-            },
-            10,
-            'title goods_img shop_price',
-          );
-          //4、扩展以前对象的属性
-          middleNavResult[i].subGoods = relationGoodsArr;
-        } catch (error) {
-          middleNavResult[i].subGoods = [];
+    let middleNavResult = await this.cacheService.get('indexMiddleNav');
+    if (!middleNavResult) {
+      middleNavResult = await this.navService.find({
+        position: 2,
+        status: 1,
+      });
+      //middleNavResult 不可改变对象 (坑)  改为可改变对象
+      middleNavResult = JSON.parse(JSON.stringify(middleNavResult));
+      for (let i = 0; i < middleNavResult.length; i++) {
+        if (middleNavResult[i].relation) {
+          try {
+            //1. relation转化为数组
+            const tempArr = middleNavResult[i].relation
+              .replace(/，/g, ',')
+              .split(',');
+            const relationIdsArr = [];
+            //2、数组中的_id 转换成 Obejct _id
+            tempArr.forEach((value) => {
+              relationIdsArr.push(new mongoose.Types.ObjectId(value));
+            });
+            //3、数据库里面查找 _id 对应的数据
+            const relationGoodsArr = await this.goodsService.findIn(
+              {
+                _id: { $in: relationIdsArr },
+              },
+              10,
+              'title goods_img shop_price',
+            );
+            //4、扩展以前对象的属性
+            middleNavResult[i].subGoods = relationGoodsArr;
+          } catch (error) {
+            middleNavResult[i].subGoods = [];
+          }
         }
       }
+      this.cacheService.set('indexMiddleNav', middleNavResult, 60 * 60);
     }
+
     //获取手机分类下面的子分类
-    const phoneResult = await this.goodsService.getCategoryGoods(
-      '628f9e83b2ee409d49160e20',
-      'hot',
-      8,
-    );
+    let phoneResult = await this.cacheService.get('indexPhone');
+    if (!phoneResult) {
+      phoneResult = await this.goodsService.getCategoryGoods(
+        '628f9e83b2ee409d49160e20',
+        'hot',
+        8,
+      );
+      this.cacheService.set('indexPhone', phoneResult, 60 * 60);
+    }
+
     //获取tv 分类下子分类 629b77702daaab33f8433baa
-    const tvResult = await this.goodsService.getCategoryGoods(
-      '629b77702daaab33f8433baa',
-      'hot',
-      10,
-    );
+    let tvResult = await this.cacheService.get('indexTv');
+    if (!tvResult) {
+      tvResult = await this.goodsService.getCategoryGoods(
+        '629b77702daaab33f8433baa',
+        'hot',
+        10,
+      );
+      this.cacheService.set('indexTv', tvResult, 60 * 60);
+    }
     return {
       topNav: topNavResult,
       focus: focusResult,
