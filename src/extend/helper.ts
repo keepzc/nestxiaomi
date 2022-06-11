6297f187c1eed57509f1f93bbb574595927959ca
zhaochao <494979582@qq.com>
2022-06-11 14:35:19 +0800

    使用showdown插件规范规格参数

--- src/extend/helper.ts
+++ src/extend/helper.ts
@@ -1,5 +1,7 @@
 import { format } from 'silly-datetime';
 import { extname } from 'path';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const showdown = require('showdown');
 export class Helper {
   static title = '全局';
   static substring(str: string, start: number, end: number) {
@@ -16,5 +18,10 @@ export class Helper {
   static formatImg(dir, width, height) {
     height = height || width;
     return dir + '_' + width + 'x' + height + extname(dir);
+  }
+  static formatAttr(str) {
+    const converter = new showdown.Converter();
+    const html = converter.makeHtml(str);
+    return html;
   }
 }
