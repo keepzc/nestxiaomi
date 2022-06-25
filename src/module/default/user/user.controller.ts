ab49776bdfaceb10e5f99ebfbc5d4290fb7d851c
zhaochao <494979582@qq.com>
2022-06-25 17:46:44 +0800

    订单状态显示，聚合管道模糊查询

--- src/module/default/user/user.controller.ts
+++ src/module/default/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Render, Request } from '@nestjs/common';
 import { OrderService } from '../../../service/order/order.service';
+import { OrderItemService } from '../../../service/order-item/order-item.service';
 import { CookieService } from '../../../service/cookie/cookie.service';
 import * as mongoose from 'mongoose';
 
@@ -8,6 +9,7 @@ export class UserController {
   constructor(
     private orderService: OrderService,
     private cookieService: CookieService,
+    private orderItemService: OrderItemService,
   ) {}
   @Get('welcome')
   @Render('default/user/welcome')
@@ -19,9 +21,31 @@ export class UserController {
   async order(@Request() req) {
     const uid = this.cookieService.get(req, 'userinfo')._id;
     const page = req.query.page || 1;
+    const order_status = req.query.order_status || -1;
     const pageSize = 2;
+    const keywords = req.query.keywords;
     //查询当前用户下面的所有订单
-    const json = { uid: new mongoose.Types.ObjectId(uid) };
+    let json = { uid: new mongoose.Types.ObjectId(uid) };
+    if (order_status != -1) {
+      json = Object.assign(json, { order_status: parseInt(order_status) });
+    }
+    //关键词删选
+    if (keywords) {
+      const orderItemJson = Object.assign(
+        { uid: new mongoose.Types.ObjectId(uid) },
+        { product_title: { $regex: new RegExp(keywords) } },
+      );
+      const orderItemResult = await this.orderItemService.find(orderItemJson);
+      if (orderItemResult.length > 0) {
+        const tempArr = [];
+        orderItemResult.forEach((value) => {
+          tempArr.push(new mongoose.Types.ObjectId(value.order_id));
+        });
+        json = Object.assign(json, {
+          _id: { $in: tempArr },
+        });
+      }
+    }
     // 总数量
     const totalNum = await this.orderService.count(json);
     //聚合管道要注意顺序
@@ -51,6 +75,8 @@ export class UserController {
       list: result,
       totalPages: Math.ceil(totalNum / pageSize),
       page,
+      order_status,
+      keywords,
     };
   }
 
